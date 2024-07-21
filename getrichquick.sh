b4e21eff53faf61949747fd391a962930f218b00
Jyotiraditya Singh <77264025+jyotir07@users.noreply.github.com>
2024-07-21 13:12:59 +0530

    Revision file, takes name and age as input

---
+++ getrichquick.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "What is your name?"
+read name
+
+echo "What is your age?"
+read age
+
+echo "Hello, $name, you are $age years old!"
+
+
+echo "$SHELL, $HOSTNAME, $PWD"
+echo "$twitter is great, $okay"
