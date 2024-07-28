21c4e54e697543d4fc7041cd9b04714328919dba
Jyotiraditya Singh <77264025+jyotir07@users.noreply.github.com>
2024-07-28 13:35:31 +0530

    Eldenring in 144p (first boss)

---
+++ eldenring.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "You died"
+
+
+#First beast battle
+
+beast=$(( $RANDOM % 2 ))
+
+echo "Your first beast appeared. Pick a number between 0-1. (0/1)"
+
+read tarnished
+
+if [[ $beast == $tarnished ]]; then echo "Beast VANQUISHED! Congrats fellow tarnished"
+else
+	echo "You died"
+fi
+
