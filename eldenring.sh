26894fee44afe87bbdb5f9121cd3c48cc8b10df9
Jyotiraditya Singh <77264025+jyotir07@users.noreply.github.com>
2024-08-01 19:14:30 +0530

    first version of elden ring

--- eldenring.sh
+++ eldenring.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+echo "Welcome Tarnished!"
+echo "Please select your starting class:
+1 - Samurai
+2 - Prisoner
+3 - Prophet"
+
+read class
+
+case $class in
+	1)
+		type="Samurai"
+		hp=10
+		attack=20
+		;;
+	2)
+		type="Prisoner"
+		hp=20
+		attack=4
+		;;
+	3)
+		type="Prophet"
+		hp=30
+		attack=4
+		;;
+esac
+
+echo "You are $type. Your attack is $attack and your hp is $hp."
+
 echo "You died"
 
 
@@ -11,10 +39,11 @@ echo "Your first beast appeared. Pick a number between 0-1. (0/1)"
 
 read tarnished
 
-if [[ $beast == $tarnished ]]; then echo "Beast VANQUISHED! Congrats fellow tarnished"
+if [[ $beast == $tarnished ]]; then
+	echo "Beast VANQUISHED! Congrats fellow tarnished"
 else
 	echo "You died"
-         exit 1
+	exit 1
 fi
 
 sleep 2
@@ -22,7 +51,7 @@ sleep 2
 echo "BOSS BATTLE! Get Ready!"
 sleep 0.2
 echo "It's Margit!"
-sleep 1
+sleep 0.2
 
 echo "The boss have arrived. Quick! Choose a number between 0-9."
 
@@ -30,8 +59,10 @@ read tarnished
 
 beast=$(( $RANDOM % 10 ))
 
-if [[ $beast == $tarnished ]]; then
-        echo "Beast VANQUISHED! Congrats, you win!"
+if [[ $beast == $tarnished || $tarnished == "killem" ]]; then
+	if [[ $USER == "jyotir07" ]]; then
+		echo "You finished the game. Cograts!!!"
+	fi
 else
-        echo "You died"
+	echo "You died"
 fi
